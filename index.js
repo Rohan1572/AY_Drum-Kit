20ed125fad67beb4c611d4ec22835904cc04c206
Rohan1572 <hellorohan02@gmail.com>
2021-10-15 23:33:24 +0530

    last

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-for (var i = 0; i < 7; i++)
+for (var i = 0; i < 7; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
+}
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -41,4 +44,12 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);  
 }
